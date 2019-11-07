9e87478fa2ec57eb9cff8bb2db61a481d578beab
Ivan Liang <ivanwenliang@gmail.com>
2019-11-06 19:18:03 -0800

    Add create user details

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const { check, validationResult } = require("express-validator");
 const express = require("express");
+const gravatar = require("gravatar");
 const router = express.Router();
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    Register new user
@@ -13,12 +15,33 @@ router.post(
       .isEmpty(),
     check("email", "Please include a valid email").isEmail()
   ],
-  (req, res) => {
+  async (req, res) => {
+    const { name, email } = req.body;
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    try {
+      let user = await User.query().where("email", email);
+      if (user.length > 0) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = await User.query().insert({ name, email, avatar });
+      res.send("User created");
+    } catch (error) {
+      res.status(500).send("Server error");
+    }
   }
 );
 
