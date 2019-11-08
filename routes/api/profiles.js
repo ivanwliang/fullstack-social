3a855ab596b5043227a7e924ea28ac83e291e91a
Ivan Liang <ivanwenliang@gmail.com>
2019-11-07 20:34:49 -0800

    Stub out GET user profile route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const secured = require("../../middleware/secured");
 
-// @route   GET api/profiles/
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile route"));
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+
+// @route   GET api/profiles/:user_id
+// @desc    Get current user's profile
+// @access  Private
+router.get("/:user_id", secured(), async (req, res) => {
+  try {
+    // Grab user's profile by their user_id -> profile.id
+    // Also grab the nickname and avatar of the user from Users table
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
