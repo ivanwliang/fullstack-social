0d00c47c0e88df6f87b183c28a66e47611c73c95
Ivan Liang <ivanwenliang@gmail.com>
2019-11-08 17:19:00 -0800

    Add route to create new profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const secured = require("../../middleware/secured");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 const Profile = require("../../models/Profile");
@@ -26,5 +27,29 @@ router.get("/:userId", secured(), async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   POST api/profiles/:user_id
+// @desc    Create new profile for current user
+// @access  Private
+router.post(
+  "/:userId",
+  [
+    secured(),
+    [
+      check("role", "Role field is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills field is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
