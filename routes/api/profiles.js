eb7f779d7ad6147356409d839157f7342b2ec18d
Ivan Liang <ivanwenliang@gmail.com>
2019-11-11 16:50:36 -0800

    Stub out update user profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -74,28 +74,6 @@ router.post(
         .where("user_id", req.params.userId)
         .eager("users");
 
-      // if (profile) {
-      //   // Update profile
-      //   profile = await Profile.query()
-      //     .where("user_id", req.params.userId)
-      //     .patch({
-      //       user_id: req.user.user_id,
-      //       employer,
-      //       website,
-      //       location,
-      //       role,
-      //       bio,
-      //       github_username: githubUsername,
-      //       youtube_url: youtubeURL,
-      //       facebook_url: facebookURL,
-      //       twitter_url: twitterURL,
-      //       linkedin_url: linkedinURL,
-      //       instagram_url: instagramURL
-      //     });
-      //   // TODO Update skills table for user
-      //   return res.status(200).json(profile);
-      // }
-
       // Create new profile
       profile = await Profile.query().insert({
         user_id: req.user.user_id,
@@ -114,7 +92,79 @@ router.post(
       return res.send(profile);
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("Server Error");
+      return res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   PATCH api/profiles/:user_id
+// @desc    Update profile for current user
+// @access  Private
+router.patch(
+  "/:userId",
+  [
+    secured(),
+    [
+      check("role", "Role field is required")
+        .not()
+        .isEmpty()
+      // check("skills", "Skills field is required")
+      //   .not()
+      //   .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      employer,
+      website,
+      location,
+      role,
+      bio,
+      githubUsername,
+      skills,
+      youtubeURL,
+      facebookURL,
+      twitterURL,
+      instagramURL,
+      linkedinURL
+    } = req.body;
+
+    try {
+      let profile = await Profile.query()
+        .where("user_id", req.params.userId)
+        .eager("users");
+
+      if (profile) {
+        // Update profile
+        profile = await Profile.query()
+          .where("user_id", req.params.userId)
+          .patch({
+            user_id: req.user.user_id,
+            employer,
+            website,
+            location,
+            role,
+            bio,
+            github_username: githubUsername,
+            youtube_url: youtubeURL,
+            facebook_url: facebookURL,
+            twitter_url: twitterURL,
+            linkedin_url: linkedinURL,
+            instagram_url: instagramURL
+          });
+        // TODO Update skills table for user
+        return res.send(profile);
+      } else {
+        return res.status(404).send("No such user exists");
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).send("Server Error");
     }
   }
 );
